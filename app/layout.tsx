5639397a8f446d60d1aa624735ae0554780aa071
Juan Villalba <juanvillalba@MacBook-Air-de-Juan.local>
2026-04-28 13:12:02 -0500

    feat: configure open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,34 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://redrideticket.vectoros.co"),
+
   title: "Red Ride Ticket",
   description: "A venture execution studio.",
+
+  openGraph: {
+    title: "Red Ride Ticket",
+    description: "A venture execution studio.",
+    url: "https://redrideticket.vectoros.co",
+    siteName: "Red Ride Ticket",
+    type: "website",
+    images: [
+      {
+        url: "https://redrideticket.vectoros.co/red-ride-ticket-og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Red Ride Ticket OG",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Red Ride Ticket",
+    description: "A venture execution studio.",
+    images: ["https://redrideticket.vectoros.co/red-ride-ticket-og.jpg"],
+  },
 };
 
 export default function RootLayout({
